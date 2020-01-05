e75fe78883e87918fd14fd0c7877f5e564b66cf9
sureshui1990 <sureshui1990@gmail.com>
2020-01-05 14:42:29 -0600

    File replace for extension working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+// import packages
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client';
 
-const App = () => (
-  <>
-    <h3>app</h3>
-  </>
-);
+
+// Making the App component
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      endPoint: 'http://192.168.47.83:4001',
+      color:'#fff'
+    }
+  }
+  
+  send = () => {
+    const { endPoint , color } = this.state;
+    const socket = socketIOClient(endPoint);  
+    socket.emit('change color', color);
+  }
+  setColor = (color) => {
+    this.setState({ color });
+  }
+
+  render() {
+    const { endPoint } = this.state;
+    const socket = socketIOClient(endPoint);
+    socket.on('change color', color => {
+      console.log('render',color);
+      document.body.style.backgroundColor = color;
+    } );
+    return (
+      <div>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={this.send}>Change Color</button>
+
+        <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+        <button id="red" onClick={() => this.setColor('red')}>Red</button>
+
+      </div>
+      </div>
+    )
+  }
+}
 
 export default App;
