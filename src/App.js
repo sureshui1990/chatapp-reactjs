15528b9b29e5333ed93b6afc2544c34f16c5659f
sureshui1990 <sureshui1990@gmail.com>
2020-01-05 02:09:46 -0600

    File have been formated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-// import packages
-import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import React from 'react';
 
-// Making the App component
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color:'#fff'
-    }
-  }
-  
-  setColor = (color) => {
-    this.setState({ color });
-  }
-
-  render() {
-    
-    return (
-      <div>
-        <Button circular size="massive" color="orange" content="dd" inverted secondary  />
-        <Button active as="h5" >active me</Button>
-
-      </div>
-    )
-  }
-}
+const App = () => (
+  <>
+    <h3>app</h3>
+  </>
+);
 
 export default App;
